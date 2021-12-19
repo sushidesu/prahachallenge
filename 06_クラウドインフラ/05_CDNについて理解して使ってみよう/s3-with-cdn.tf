4aaafe171d9f39f30778ca1d047e753284065c5a
sushidesu <hugemerge@gmail.com>
2021-12-19 15:25:46 +0900

    cloudfrontを有効化

--- 06_クラウドインフラ/05_CDNについて理解して使ってみよう/s3-with-cdn.tf
+++ 06_クラウドインフラ/05_CDNについて理解して使ってみよう/s3-with-cdn.tf
@@ -9,6 +9,7 @@ provider "aws" {
 
 locals {
   name = "praha-task-cdn"
+  origin_id = "praha-task-cloudfront"
 }
 
 resource "aws_s3_bucket" "very-distant-storage" {
@@ -42,5 +43,44 @@ data "aws_iam_policy_document" "allow_access_from_all_users" {
     resources = [
       "${aws_s3_bucket.very-distant-storage.arn}/*",
     ]
+  }
+}
+
+// cloudfront
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.very-distant-storage.bucket_regional_domain_name
+    origin_id   = local.origin_id
+  }
+
+  enabled = true
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations = ["JP"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods = ["HEAD", "GET"]
+    cached_methods = ["HEAD", "GET"]
+    target_origin_id = local.origin_id
+    viewer_protocol_policy = "allow-all"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  tags = {
+    Name = local.name
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
