5b6cb837b87715709940bdf414e9d582be5647a8
sushidesu <hugemerge@gmail.com>
2021-12-30 21:11:22 +0900

    add background

--- 05_フロントエンド/landing-page/src/components/template/Home/Home.tsx
+++ 05_フロントエンド/landing-page/src/components/template/Home/Home.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/Button"
 
+import styles from "./Home.module.css"
+
 export const Home = (): JSX.Element => {
   return (
-    <div>
+    <div className={styles["wrapper"]}>
       <p>hello</p>
       <Button>OK</Button>
     </div>
