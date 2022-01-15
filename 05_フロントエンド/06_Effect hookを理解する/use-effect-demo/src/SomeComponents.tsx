6545edad5e569b732607076981134607162f918c
sushidesu <hugemerge@gmail.com>
2022-01-15 17:46:02 +0900

    無限ループバージョンを作成

--- 05_フロントエンド/06_Effect hookを理解する/use-effect-demo/src/SomeComponents.tsx
+++ 05_フロントエンド/06_Effect hookを理解する/use-effect-demo/src/SomeComponents.tsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react"
+
 type SomeComponentProps = {
   flag: boolean
 }
 
 export const SomeComponent = ({ flag }: SomeComponentProps) => {
+  // 無限ループバージョン
+  // stateの更新にはレンダリングが伴うため、何度もuseEffectが呼ばれてしまう
+  // 初回レンダリング -> useEffect(ステートの更新) -> ステートが更新されたのでレンダリング -> useEffect(ステートの更新)
+  const [rendered, setRendered] = useState(0)
+
+  useEffect(() => {
+    setRendered(prev => prev + 1)
+  })
+  // 無限ループバージョンここまで
+
   return (
-    <p>ここに、このコンポーネントがレンダリングされた回数を表示してみよう!</p>
+    <div>
+      <p>ここに、このコンポーネントがレンダリングされた回数を表示してみよう!</p>
+      <p>{`${rendered} times`}</p>
+    </div>
   )
 }
