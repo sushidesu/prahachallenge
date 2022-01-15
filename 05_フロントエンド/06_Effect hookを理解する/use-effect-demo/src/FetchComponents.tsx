3477f4cd774aff88ee0327cba2ec72951830d82e
sushidesu <hugemerge@gmail.com>
2022-01-15 18:36:35 +0900

    FetchCompomentを実装

--- 05_フロントエンド/06_Effect hookを理解する/use-effect-demo/src/FetchComponents.tsx
+++ 05_フロントエンド/06_Effect hookを理解する/use-effect-demo/src/FetchComponents.tsx
@@ -1,17 +1,66 @@
 import { useEffect, useState } from "react"
 
 export const FetchComponent = () => {
-  const [data, setData] = useState({
-    subscribers: 0,
-    stars: 0,
-  })
-
   // ここでuseEffectを使ってstar数を取得してみましょう!
+  const repoData = useFetch(() => fetchGithubAPI("facebook/react"))
 
   return (
     <div>
       <p>ここにReactのGitHubレポジトリに付いたスターの数を表示してみよう</p>
-      <p>{data.stars} stars</p>
+      {
+        repoData.status === "loading"
+          ? <p>ロード中...</p>
+          : <p>{repoData.value.stargazers_count} stars</p>
+      }
     </div>
   )
+}
+
+type GithubAPIResponse = {
+  id: string
+  full_name: string
+  stargazers_count: number
+}
+
+/**
+ * 簡易的なGithubAPIクライアント
+ */
+const fetchGithubAPI = async (name: string) => {
+  const result = await fetch("https://api.github.com/repos/" + name)
+  return result.json() as Promise<GithubAPIResponse>
+}
+
+type Loadable<T> = {
+  status: "loading"
+} | {
+  status: "completed"
+  value: T
+}
+
+/**
+ * 非同期なデータを取得するhooks
+ */
+function useFetch<T>(fetcher: () => Promise<T>): Loadable<T> {
+  const [data, setData] = useState<Loadable<T>>({ status: "loading" })
+
+  useEffect(() => {
+    let unmounted = false
+
+    const doFetch = async () => {
+      const result = await fetcher()
+      if (!unmounted) {
+        setData({
+          status: 'completed',
+          value: result
+        })
+      }
+    }
+    doFetch()
+
+    return () => {
+      unmounted = true
+    }
+  }, [])
+
+  return data
 }
