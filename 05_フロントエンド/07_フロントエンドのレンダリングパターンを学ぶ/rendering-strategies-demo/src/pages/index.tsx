14a95191dc6a972012ca47f144ab9d892d2a334a
sushidesu <hugemerge@gmail.com>
2022-01-16 14:09:31 +0900

    create CSR page

--- 05_フロントエンド/07_フロントエンドのレンダリングパターンを学ぶ/rendering-strategies-demo/src/pages/index.tsx
+++ 05_フロントエンド/07_フロントエンドのレンダリングパターンを学ぶ/rendering-strategies-demo/src/pages/index.tsx
@@ -1,15 +1,30 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { getRepoData } from "../lib/github-api-client"
+import { useFetchOnce } from "../lib/useFetchOnce"
 
 const Home: NextPage = () => {
+  const repoData = useFetchOnce(() => getRepoData("facebook/react"))
+
   return (
     <div>
       <Head>
-        <title>Rendering Demo</title>
+        <title>Rendering Demo (CSR)</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <p>hello</p>
+        <div>
+          <p>hello</p>
+          {repoData.status === "loading"
+            ? <p>loading...</p>
+            : (
+              <div>
+                <p>{`name: ${repoData.value.full_name}`}</p>
+                <p>{`stars: ${repoData.value.stargazers_count}`}</p>
+              </div>
+            )
+          }
+        </div>
       </main>
     </div>
   )
