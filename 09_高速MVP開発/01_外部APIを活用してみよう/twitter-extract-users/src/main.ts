bc950f84e592cd10c0464093e8300f13f15dee40
sushidesu <hugemerge@gmail.com>
2022-04-02 18:38:31 +0900

    基本的なコマンドを定義

--- 09_高速MVP開発/01_外部APIを活用してみよう/twitter-extract-users/src/main.ts
+++ 09_高速MVP開発/01_外部APIを活用してみよう/twitter-extract-users/src/main.ts
@@ -1,8 +1,45 @@
 #!/usr/bin/env node
-import { add } from "./add"
+import { program } from "commander"
 
 const main = () => {
-  console.log(`1 + 1 = ${add(1, 1)}`)
+  program
+    .command("hello")
+    .description("say hello")
+    .action(() => {
+      console.log("hello!")
+    })
+
+  program
+    .command("text")
+    .description("extract users who tweeted a specific word")
+    .argument("<string>", "string for query")
+    .action((arg) => {
+      if (!isString(arg)) {
+        console.log("<string> is required")
+        return
+      }
+
+      console.log(`query by text: ${arg}`)
+    })
+
+  program
+    .command("following")
+    .description("extract users who are following a specific user")
+    .argument("<userId>", "user id for query")
+    .action((arg) => {
+      if (!isString(arg)) {
+        console.log("<userId> is required")
+        return
+      }
+
+      console.log(`query by following user: ${JSON.stringify(arg)}`)
+    })
+
+  program.parse()
+}
+
+const isString = (value: unknown): value is string => {
+  return typeof value === "string"
 }
 
 main()
