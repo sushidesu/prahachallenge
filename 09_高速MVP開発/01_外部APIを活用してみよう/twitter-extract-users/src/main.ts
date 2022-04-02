435859be02ee4282b58aa4dcbf19a3ff87571fda
sushidesu <hugemerge@gmail.com>
2022-04-02 21:33:09 +0900

    特定のユーザーをフォロー中のユーザー検索を実装

--- 09_高速MVP開発/01_外部APIを活用してみよう/twitter-extract-users/src/main.ts
+++ 09_高速MVP開発/01_外部APIを活用してみよう/twitter-extract-users/src/main.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import { program } from "commander"
 import { TwitterClient } from "./client/twitter-client"
+import { ExtractUsersByFollowing } from "./usecase/extract-users-by-following"
 import { ExtractUsersByText } from "./usecase/extract-users-by-text"
+import { UserDTO } from "./usecase/user-dto"
 
 const main = () => {
   program
@@ -25,25 +27,31 @@ const main = () => {
 
       console.log(`users who tweeted ${arg}:`)
       const result = await extractByText.exec(arg)
-      console.log(
-        result.map((u) => `${u.props.name} (@${u.props.username})`).join("\n")
-      )
+      console.log(format(result))
     })
 
   program
     .command("following")
     .description("extract users who are following a specific user")
     .argument("<userId>", "user id for query")
-    .action((arg) => {
+    .action(async (arg) => {
       if (!isString(arg)) {
         console.log("<userId> is required")
         return
       }
+      const twitterClient = new TwitterClient()
+      const extractByFollowing = new ExtractUsersByFollowing(twitterClient)
 
-      console.log(`query by following user: ${JSON.stringify(arg)}`)
+      console.log(`users who following ${arg}:`)
+      const result = await extractByFollowing.exec(arg)
+      console.log(format(result))
     })
 
   program.parse()
+}
+
+const format = (users: UserDTO[]): string => {
+  return users.map((u) => `${u.props.name} (@${u.props.username})`).join("\n")
 }
 
 const isString = (value: unknown): value is string => {
