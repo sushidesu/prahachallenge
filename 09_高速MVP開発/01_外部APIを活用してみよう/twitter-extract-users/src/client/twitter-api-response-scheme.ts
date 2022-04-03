8a4d5760bef8536e40d9b99b605e8a803f2d7371
sushidesu <hugemerge@gmail.com>
2022-04-03 11:59:03 +0900

    TwitterClient.getUsersByText()を実装

---
+++ 09_高速MVP開発/01_外部APIを活用してみよう/twitter-extract-users/src/client/twitter-api-response-scheme.ts
@@ -0,0 +1,20 @@
+import { z } from "zod"
+
+const twitterUserScheme = z.object({
+  id: z.string().nonempty(),
+  name: z.string(),
+  username: z.string().nonempty(),
+})
+
+const tweetScheme = z.object({
+  author_id: z.string().nonempty(),
+  id: z.string().nonempty(),
+  text: z.string(),
+})
+
+export const twitterRecentTweetsResponseScheme = z.object({
+  data: z.array(tweetScheme),
+  includes: z.object({
+    users: z.array(twitterUserScheme),
+  }),
+})
--- 09_高速MVP開発/01_外部APIを活用してみよう/twitter-extract-users/src/client/twitter-client.ts
+++ 09_高速MVP開発/01_外部APIを活用してみよう/twitter-extract-users/src/client/twitter-client.ts
@@ -1,16 +1,41 @@
+import "dotenv/config"
 import { ITwitterClient } from "../domain/twitter-user/twitter-client-interface"
 import { TwitterUser } from "../domain/twitter-user/twitter-user"
+import { twitterRecentTweetsResponseScheme } from "./twitter-api-response-scheme"
 
 export class TwitterClient implements ITwitterClient {
+  private readonly token: string
+  private readonly API_ORIGIN = "https://api.twitter.com/2"
+
+  constructor() {
+    // .envからBEARER_TOKENを読み込む
+    const token = process.env["BEARER_TOKEN"]
+    if (token === undefined) {
+      throw new Error("env.BEARER_TOKEN is required")
+    }
+    this.token = token
+  }
+
   async getUsersByTweet(text: string): Promise<TwitterUser[]> {
-    console.info(`TwitterClient.getUsersByTweet()`, text)
-    return [
-      {
-        id: "test-user",
-        name: "test",
-        username: "tteesstt",
+    const query = new URLSearchParams({
+      query: text,
+      expansions: "author_id",
+      "user.fields": "id,name,username",
+    })
+    const url = `${this.API_ORIGIN}/tweets/search/recent?${query.toString()}`
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
       },
-    ]
+    })
+    const result = await response.json()
+    const parsed = twitterRecentTweetsResponseScheme.parse(result)
+
+    return parsed.includes.users.map((u) => ({
+      id: u.id,
+      name: u.name,
+      username: u.username,
+    }))
   }
 
   async getUsersByFollowing(followingUserId: string): Promise<TwitterUser[]> {
